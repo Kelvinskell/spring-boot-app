db79e6b7a76643b94ef3ac5491e892bfb693bd05
Kelvinskell <kelvinskelll@gmail.com>
2024-10-10 17:37:51 +0100

    decalre mysqldb variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,13 @@ variable "app" {
 variable "image_name" {
   type = string
 }
+
+variable "db_username" {
+  type = string
+  sensitive = true
+}
+
+variable "db_password" {
+  type = string
+  sensitive = true
+}
